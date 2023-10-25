2be03288aa6336915427c98142d3601f3fb62075
YosefCode <yusefdevops@outlook.com>
2023-10-25 20:27:24 +0200

    mavensample1/

---
+++ mavensample1/src/main/java/mavensample1/mavensample1/App.java
@@ -0,0 +1,13 @@
+package mavensample1.mavensample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
